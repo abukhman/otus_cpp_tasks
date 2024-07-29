9753f8c525cdecd78647d349089155eda40885fb
Anton Bukhman <a.bukhman@mts.ai>
2024-07-29 22:28:39 +0300

    Add handling memory allocation fails

--- alloc.h
+++ alloc.h
@@ -8,18 +8,31 @@ struct my_allocator {
     my_allocator () noexcept {
 	    buf = malloc(kb * 1024);
 	    pos = 0;
+	    if (buf == nullptr) {
+              cur_size = 0;
+	      return;
+	    }
 	    cur_size = kb;
     }
     T* allocate (std::size_t n)
     {
+	    if (cur_size < n) {
+		   throw(std::bad_alloc{});
+	    }
 	    std::cout<<"allocate "<<n<<" "<<sizeof(T)<<" "<<buf<<std::endl;
 	    void* ret_adr = static_cast<T*>(buf);
 	    buf = static_cast<void *> (static_cast<T*>(buf) + n);
+	    cur_size -= n;
 	    return static_cast<T*>(ret_adr);
     }
     void deallocate (T* p, std::size_t n)
     {
 	    std::cout<<"deallocate "<<n<<std::endl;
+    }
+    ~my_allocator() noexcept {
+      if (buf != nullptr) {
+        free(buf);
+      }
     }
 };
 
